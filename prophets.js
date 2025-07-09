741c2ff554cf1edc6b8af088dbefcde2db09bb78
andrade-lopes <jlopes@byupathway.edu>
2025-07-09 22:49:24 -0100

    CSS and JS updated

--- prophets.js
+++ prophets.js
@@ -10,11 +10,15 @@ async function getProphetData() {
 
 const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
-        let card = document.createElement('section');
-        let fullName = document.createElement('h2');
-        let portrait = document.createElement('img');
+        const card = document.createElement('section');
+        const fullName = document.createElement('h2');
+        const birthdate = document.createElement('p');
+        const birthplace = document.createElement('p');
+        const portrait = document.createElement('img');
 
         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
 
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
@@ -23,10 +27,10 @@ const displayProphets = (prophets) => {
         portrait.setAttribute('height', '440');
 
         card.appendChild(fullName);
+        card.appendChild(birthdate);
+        card.appendChild(birthplace);
         card.appendChild(portrait);
 
         cards.appendChild(card);
     });
-}
-
-getProphetData();
+};
