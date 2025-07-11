5234ef3786667ff6519c70888cac9b6bd72189b7
andrade-lopes <jlopes@byupathway.edu>
2025-07-10 23:09:25 -0100

    files updated

--- prophets.js
+++ prophets.js
@@ -4,7 +4,7 @@ const cards = document.querySelector('#cards');
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    // console.table(data.prophets); // para debug
+    // console.table(data.prophets); // for debug
     displayProphets(data.prophets);
 }
 
