abb30507d2ed008eab4da560880f52753b03ce1f
andrade-lopes <jlopes@byupathway.edu>
2025-08-13 22:06:48 -0100

    Learn more botton update

--- finalproject/main.js
+++ finalproject/main.js
@@ -25,3 +25,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     localStorage.setItem("lastVisit", now);
 });
+
+// Pega os elementos
+const modal = document.getElementById("myModal");
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.querySelector(".close");
+
+// Abrir modal
+openBtn.onclick = function (e) {
+    e.preventDefault(); // evita que o link recarregue a página
+    modal.style.display = "block";
+}
+
+// Fechar modal
+closeBtn.onclick = function () {
+    modal.style.display = "none";
+}
+
+// Fechar clicando fora do conteúdo
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
